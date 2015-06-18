098ef0627e976149c99b3f8d097272865d7ae5c5
hamneggs <gerarddgeer@gmail.com>
2015-06-18 06:35:56 -0400

    Fixed the capitalization bug. Now search queries no longer lose their capitalization on submit.

--- Server/webs/test/js/app.js
+++ Server/webs/test/js/app.js
@@ -40,11 +40,11 @@ app.controller('QueryController', ['$animate', function($animate){
 			
 		this.submitted = true;
 		this.response = Array();
-		this.query = this.query.toLowerCase();
+		lowercase = this.query.toLowerCase();
 		for(var i = 0; i < testLights.length; ++i)
 		{
-			if(	testLights[i].name.toLowerCase().indexOf(this.query) >= 0 ||
-			testLights[i].client.toLowerCase().indexOf(this.query) >= 0 )
+			if(	testLights[i].name.toLowerCase().indexOf(lowercase) >= 0 ||
+			testLights[i].client.toLowerCase().indexOf(lowercase) >= 0 )
 				this.response.push(testLights[i]);
 		}
 	};
