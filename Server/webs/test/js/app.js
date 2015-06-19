1b182e0d08dd7f45f2b5731facf44d1067e35bae
hamneggs <gerarddgeer@gmail.com>
2015-06-19 08:04:29 -0400

    Added some now unused slider files, but mainly got preset buttons working and done. Also made the mouse over animation retreat into the button instead of it protruding out.

--- Server/webs/test/js/app.js
+++ Server/webs/test/js/app.js
@@ -1,5 +1,5 @@
-// Create our Angular module, which depends on ngAnimate.
-var app = angular.module('LumaClient', ['ngAnimate']);
+// Create our Angular module.
+var app = angular.module('LumaClient', ['rzModule']);
 
 // Just a simple bank of test lights.
 var testLights = [ 	{name:"Gerard's Desk", client:"Gerard and Josh's Room"},
@@ -95,24 +95,187 @@ app.controller('ResultController', ['$rootScope', function($rootScope){
 	paradigm editing widget.
 */
 app.controller('WaveformController', function($scope){
-	
+	$scope.$broadcast('refreshSlider')
 	// The currently selected channel.
 	$scope.channel = 'red';
 	
+	// The periods of the channel waveforms.
+	$scope.rPeriod = 0.5;
+	$scope.gPeriod = 0.5;
+	$scope.bPeriod = 0.5;
+	
+	// Whether or not the mouse is down, so that we can
+	// distinguish between moving and dragging.
+	var mdown = false;
+	
+	// The three canvases.
+	$scope.rCanvas = document.getElementById('red_canvas');
+	$scope.gCanvas = document.getElementById('green_canvas');
+	$scope.bCanvas = document.getElementById('blue_canvas');
+	
 	// The various canvases' rendering contexts.
-	$scope.rCtx = document.getElementById('red_canvas').getContext('2d');	
-	$scope.gCtx = document.getElementById('green_canvas').getContext('2d');	
-	$scope.bCtx = document.getElementById('blue_canvas').getContext('2d');
+	$scope.rCtx = $scope.rCanvas.getContext('2d');	
+	$scope.gCtx = $scope.gCanvas.getContext('2d');	
+	$scope.bCtx = $scope.bCanvas.getContext('2d');
 	
-	$scope.rCtx.rect(0, 0, 800, 450);
-	$scope.rCtx.fillStyle='red';
-	$scope.rCtx.fill();
+	$scope.rCtx.strokeStyle = '#FF0000';
+	$scope.gCtx.strokeStyle = '#00FF00';
+	$scope.bCtx.strokeStyle = '#0000FF';
 	
-	$scope.gCtx.rect(0, 0, 800, 450);
-	$scope.gCtx.fillStyle='green';
-	$scope.gCtx.fill();
+	// The arrays of brightness values.
+	$scope.rVals = Array();
+	$scope.gVals = Array();
+	$scope.bVals = Array();
 	
-	$scope.bCtx.rect(0, 0, 800, 450);
-	$scope.bCtx.fillStyle='blue';
-	$scope.bCtx.fill();
+	this.sine = function(ctx, vals, color)
+	{
+		vals.length = 0;
+		for(var i = 0; i < 128; ++i)
+		{
+			//								2PI/128
+			vals.push( Math.pow( Math.sin(i*.024544), 2 ) );
+		}
+		updateAll(ctx, vals, color);
+	}
+	this.saw = function(ctx, vals, color)
+	{
+		vals.length = 0;
+		for(var i = 0; i < 128; ++i)
+		{
+			vals.push( i/128 );
+		}
+		updateAll(ctx, vals, color);
+	}
+	this.revsaw = function(ctx, vals, color)
+	{
+		vals.length = 0;
+		for(var i = 0; i < 128; ++i)
+		{
+			vals.push( 1.0-(i/128) );
+		}
+		updateAll(ctx, vals, color);
+	}
+	this.square = function(ctx, vals, color)
+	{
+		vals.length = 0;
+		for(var i = 0; i < 128; ++i)
+		{
+			vals.push( Math.floor( 2.0*(i/128) ) );
+		}
+		updateAll(ctx, vals, color);
+	}
+	this.triangle = function(ctx, vals, color)
+	{
+		vals.length = 0;
+		for(var i = 0; i < 64; ++i)
+		{
+			vals.push( i/64 );
+		}
+		for(var i = 64; i < 128; ++i)
+		{
+			vals.push( 1.0-((i-64)/64) );
+		}
+		updateAll(ctx, vals, color);
+	}
+	// Initialize them all with dummy values.
+	for(var i = 0; i < 128; ++i)
+	{
+		$scope.rVals.push( Math.sin(i*.1)*Math.sin(i*.1) );
+		$scope.gVals.push( Math.sin(i*.05)*Math.sin(i*.05) );
+		$scope.bVals.push( Math.sin(i*.01)*Math.sin(i*.01) );
+	}
+	
+	// Redraws a line.
+	function update(ctx, index, val, color)
+	{
+		index *= 8;
+		
+		// Clear the region of the line.
+		ctx.clearRect(index-2, 0, 4, 576);
+		
+		// Draw the new line.
+		ctx.beginPath();
+		ctx.strokeStyle = color;
+		ctx.lineWidth = 2;
+		ctx.moveTo(index, 576);
+		ctx.lineTo(index, 576-576*val);
+		ctx.stroke();
+	}
+	
+	// Redraws all lines for a canvas.
+	function updateAll(ctx, vals, color)
+	{
+		for(var i = 0; i < 128; ++i)
+		{
+			update(ctx, i, vals[i], color);
+		}
+	}
+	
+	// A quick function that gets the mouse position on a canvas.
+    function getMousePos(canvas, evt) {
+		var rect = canvas.getBoundingClientRect();
+		return {
+			x: evt.clientX - rect.left,
+			y: evt.clientY - rect.top
+		};
+    }
+	
+	// Draw all the initial values to the screen.
+	updateAll($scope.rCtx, $scope.rVals, '#FF0000');
+	updateAll($scope.gCtx, $scope.gVals, '#00FF00');
+	updateAll($scope.bCtx, $scope.bVals, '#0000FF');
+	
+	// A function that takes the mouse position and finds the closest
+	// value line to it, and updates it.
+	function updateVal(canvas, ctx, evt, vals, color)
+	{
+		if(mdown)
+		{
+			// Get the mouse position.
+			var pos = getMousePos(canvas, evt);
+			// Get the index of the value to update.
+			var index = parseInt((pos.x/canvas.width)*vals.length);
+			var val = 1 - (pos.y/canvas.height);
+			// Update the value.
+			vals[index] = val;
+			
+			update(ctx, index, val, color);
+		}
+	}
+	
+	// Add the mouse up/down listeners so we can know when we're taking a drag.
+	// Oh, and we also want functionality on click, so we update on mouse-down.
+	$scope.rCanvas.addEventListener('mousedown', function(evt){
+		mdown=true;
+		updateVal($scope.rCanvas, $scope.rCtx, evt, $scope.rVals, '#FF0000');
+	},false);
+	$scope.gCanvas.addEventListener('mousedown', function(){
+		mdown=true;
+		updateVal($scope.gCanvas, $scope.gCtx, evt, $scope.gVals, '#00FF00');
+	},false);
+	$scope.bCanvas.addEventListener('mousedown', function(){
+		mdown=true;
+		updateVal($scope.bCanvas, $scope.bCtx, evt, $scope.bVals, '#0000FF');
+	},false);
+	$scope.rCanvas.addEventListener('mouseup', function(){mdown=false;},false);
+	$scope.gCanvas.addEventListener('mouseup', function(){mdown=false;},false);
+	$scope.bCanvas.addEventListener('mouseup', function(){mdown=false;},false);
+	
+	// When the mouse leaves the canvas, we want it to perk up a bit.
+	$scope.rCanvas.addEventListener('mouseout', function(){mdown=false;},false);
+	$scope.gCanvas.addEventListener('mouseout', function(){mdown=false;},false);
+	$scope.bCanvas.addEventListener('mouseout', function(){mdown=false;},false);
+	
+	
+	// Add the mouse move functions, which update the model if the mouse
+	// is dragged.
+	$scope.rCanvas.addEventListener('mousemove', function(evt){
+		updateVal($scope.rCanvas, $scope.rCtx, evt, $scope.rVals, '#FF0000');
+	},false);
+	$scope.gCanvas.addEventListener('mousemove', function(evt){
+		updateVal($scope.gCanvas, $scope.gCtx, evt, $scope.gVals, '#00FF00');
+	},false);
+	$scope.bCanvas.addEventListener('mousemove', function(evt){
+		updateVal($scope.bCanvas, $scope.bCtx, evt, $scope.bVals, '#0000FF');
+	},false);
 });
