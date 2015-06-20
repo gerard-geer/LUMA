c0f11acc567beda86c235f68119bb50cbff70bd5
hamneggs <gerarddgeer@gmail.com>
2015-06-20 03:16:54 -0400

    Removed RZSlider injection, and started pulling down angular from on high.

--- Server/webs/test/js/app.js
+++ Server/webs/test/js/app.js
@@ -1,5 +1,5 @@
 // Create our Angular module.
-var app = angular.module('LumaClient', ['rzModule']);
+var app = angular.module('LumaClient', []);
 
 // Just a simple bank of test lights.
 var testLights = [ 	{name:"Gerard's Desk", client:"Gerard and Josh's Room"},
