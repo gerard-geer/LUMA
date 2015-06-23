624e34e3fb40fdd01cf5045b3ba110487bab3682
hamneggs <gerarddgeer@gmail.com>
2015-06-23 03:17:34 -0400

    Didn't need those files yet.

--- Server/webs/test/js/searchBarDirective.js
+++
@@ -1,8 +0,0 @@
-angular.module('LUMAClient', [])
-    .directive('searchBar', function () {
-        return {          
-            restrict: 'E',
-            replace:true,
-            templateUrl:'search-bar.html'			
-        };
-    });
