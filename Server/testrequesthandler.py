0d05f1a7b3fcfa49263436eee8429e42b3395e95
hamneggs <gerarddgeer@gmail.com>
2015-06-29 02:11:21 -0400

    Got some more request handler tests in.

--- Server/testrequesthandler.py
+++ Server/testrequesthandler.py
@@ -16,32 +16,38 @@ lightQueries = [	# Test good queries from a privileged user.
 					# Test bad queries from a valid user.
 					{'uuid':'1', 'query':'not a worthwhile query'},
 					# Test good queries from a non-privileged user.
-					{'uuid':'x', 'query':'Research Room'},
-					{'uuid':'x', 'query':'Research'},
-					{'uuid':'x', 'query':'Rese'},
-					{'uuid':'x', 'query':'Ceiling'},
-					{'uuid':'x', 'query':'Under Countertop'},
-					{'uuid':'x', 'query':'Under'},
-					{'uuid':'x', 'query':'Unde'},
-					{'uuid':'x', 'query':'127.0.0'},
+					{'uuid':'X', 'query':'Research Room'},
+					{'uuid':'X', 'query':'Research'},
+					{'uuid':'X', 'query':'Rese'},
+					{'uuid':'X', 'query':'Ceiling'},
+					{'uuid':'X', 'query':'Under Countertop'},
+					{'uuid':'X', 'query':'Under'},
+					{'uuid':'X', 'query':'Unde'},
+					{'uuid':'X', 'query':'127.0.0'},
 					# Test bad queries from a non-privileged user.
 					{'uuid':'1', 'query':'not a valid query'}
+				]
 
-stateQueries = [
-					{'uuid':'1', 'id': "100001"}
-					{'uuid':'1', 'id': "100001"}
-					{'uuid':'1', 'id': "100001"}
+stateQueries = [	# Test valid state queries from a privileged user.
+					{'uuid':'1', 'id': "100001"},
+					{'uuid':'1', 'id': "100004"},
+					# Test invalid state queries from a privileged user.
+					{'uuid':'1', 'id': "10000X"},
+					# Test valid state queries from a non-privileged user.
+					{'uuid':'X', 'id': "100001"},
+					{'uuid':'X', 'id': "100004"},
+					# Test invalid state queries from a non-privileged user.
+					{'uuid':'X', 'id': "10000X"}
 				]
 
 if __name__ == '__main__':
 	rh = RequestHandler.Instance()
-	# Make all the test queries.
+	# Make all the test light queries.
 	for q in lightQueries:
-		printme = []
 		for light in rh.lightQuery(q)['lights']:
-			printme.append(light['name'])
-		print(printme)
-		
+			print(light)
+	
 	# Make all the test state queries.
 	for q in stateQueries:
 		print(rh.stateQuery(q))
+	
