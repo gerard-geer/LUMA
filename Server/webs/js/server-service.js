46f74fe3ec9b2f8b8b4c038c75e2b9c718473dca
hamneggs <gerarddgeer@gmail.com>
2015-06-29 15:50:04 -0400

    Error messages now reset upon new query.

--- Server/webs/js/server-service.js
+++ Server/webs/js/server-service.js
@@ -30,6 +30,7 @@ angular.module('LUMAClient').factory('LUMAServerService',
 	{
 		// Pre-emptively clear out the Query Result array.
 		LUMAStateService.queryResults.length = 0;
+		
 		// Go ahead and clear out the old light state and errors.
 		LUMAStateService.lightState = null;
 		LUMAStateService.isEditing = false;
