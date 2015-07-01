b853a10aa43e57da08a522ba3fb3f02fa96d203b
hamneggs <gerarddgeer@gmail.com>
2015-07-01 19:38:17 -0400

    Simple test to try multiple recv calls on the client.

--- Client/lumaclient.py
+++ Client/lumaclient.py
@@ -13,7 +13,7 @@ from datetime import datetime
 HOST = ''
 PORT = 8641
 FILE = 'config/lights.json'
-DATAREAD = 65536
+DATAREAD = 64
 TIMEOUT = 5.0
 luma = LUMA(FILE)
 
@@ -47,7 +47,11 @@ class LUMATCPHandler(BaseRequestHandler):
 		
 		# Receive some data. In Python 2 recv returns a String instead of a
 		# byte array. This makes sterilization really easy.
-		req = self.request.recv(DATAREAD)
+		chunk = self.request.recv(DATAREAD)
+		req = chunk
+		while chunk != '':
+			print(chunk)
+			req += chunk
 		print('Request:')
 		print('  Length: '+str(len(req)))
 		# Now we need to act upon the request.
