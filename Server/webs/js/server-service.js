5dac155321499046db6160daac693fc78f3e459d
hamneggs <gerarddgeer@gmail.com>
2015-07-01 19:09:58 -0400

    Removed the old teststate object. It's no longer and will no longer be used.

--- Server/webs/js/server-service.js
+++ Server/webs/js/server-service.js
@@ -1,20 +1,5 @@
 // LUMA Copyright (c) Gerard Geer 2014-2015 
 					
-var testState = {	success: true,
-					message: "simple message from client of Couch",
-					id:"561519e7-f6a8-462b-a749-c7de69ade7d1",
-					name: "Couch",
-					client: "Max's Room",
-					r_t: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-					r_v: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-					r_c: 0,
-					g_t: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-					g_v: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-					g_c: 0,
-					b_t: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-					b_v: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-					b_c: 0};
-					
 /*
 	The lUMA Server Service. This functions as a singleton that
 	encapsulates interactions with the server, and stores the
