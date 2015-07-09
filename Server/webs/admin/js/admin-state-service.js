4654346ff9325ede1d318cf787c74aadb1f0996a
hamneggs <gerarddgeer@gmail.com>
2015-07-09 01:24:08 -0400

    Added a dividing border between button and description, and started on the Angular backend of things, beginning by making a service that provides access to state to all controllers.

---
+++ Server/webs/admin/js/admin-state-service.js
@@ -0,0 +1,23 @@
+// LUMA Copyright (c) Gerard Geer 2014-2015 
+					
+/*
+	The Admin Portal's state service. This essentially serves as a globally
+	accessible singleton that stores state between disjoint controllers.
+*/
+angular.module('LUMAClient').factory('AdminStateService', [function() {
+    
+	var dialogEnum = {
+		NO_DIALOG: 0,
+		NEW_LIGHT: 1
+	}
+	return {
+		// An object in which to create and store a new light.
+		newLight: null,
+		// Whether or not an error was returned on an action.
+		isError: false,
+		// What dialog to show.
+		dialogToShow: dialogEnum.NO_DIALOG,
+		// THe dialog enumerator.
+		DIALOG_ENUM: dialogEnum
+    };
+}]);
---
+++ Server/webs/admin/js/app.js
@@ -0,0 +1,4 @@
+// LUMA Copyright (c) Gerard Geer 2014-2015 
+
+// Create our Angular module.
+var app = angular.module('LUMAClientAdminPortal', ['ngModal', 'mgo-mousetrap']);
