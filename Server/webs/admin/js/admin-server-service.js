ffb1ca3834989c08c8e6627c4a58d21419f4d187
hamneggs <gerarddgeer@gmail.com>
2015-07-09 05:20:11 -0400

    Created a server service for admin shenanigans.

---
+++ Server/webs/admin/js/admin-server-service.js
@@ -0,0 +1,14 @@
+// LUMA Copyright (c) Gerard Geer 2014-2015 
+					
+/*
+	The lUMA Server Service. This functions as a singleton that
+	encapsulates interactions with the server, and stores the
+	selected query result and the state of the requested light.
+*/
+angular.module('LUMAClientAdminPortal').factory('AdminServerService', 
+['$http', 'AdminStateService', function($http, AdminStateService) {
+	
+	
+    return {
+    };
+}]);
