2ebb8c48bccc6ed2b012dd5cfcbc26ea05fae4cf
hamneggs <gerarddgeer@gmail.com>
2015-07-12 12:07:59 -0400

    Oops forgot to give this controller a real name.

--- Server/webs/admin/js/selected-light-controller.js
+++ Server/webs/admin/js/selected-light-controller.js
@@ -4,7 +4,7 @@
 	Each light listing needs to have an on-click callback so that
 	we can raise for it a more specific light dialog.
 */
-angular.module('LUMAClientAdminPortal').controller('ListingController',
+angular.module('LUMAClientAdminPortal').controller('LightListingController',
 ['$scope', 'AdminStateService',
 function($scope, AdminStateService)
 {
