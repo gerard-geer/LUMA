6c14d55b406b20292710887feaaa5727b4a2fead
gerard-geer <gerarddgeer@gmail.com>
2015-07-15 22:45:55 -0400

    Separated the sanitation functions out from the request handler. The request handler was getting really pudgy.

--- Server/requesthandler.py
+++ Server/requesthandler.py
@@ -9,6 +9,7 @@ from json import loads
 from uuid import uuid4
 
 from singleton import Singleton
+from sanitizer import *
 
 @Singleton
 class RequestHandler(object):
@@ -49,266 +50,6 @@ class RequestHandler(object):
 		"""
 		return (self._am.load(), self._lm.load())
 	
-		
-	def _sanitizeLightQuery(self, req):
-		"""
-		Sanitizes a light query. This makes sure that a light query is a
-		JSON Dictionary, then that it has the required keys, and the data 
-		types of those keys' values	are correct.
-		
-		Parameters:
-			req (JSON): The Dictionary that contains the request.
-			
-		Returns:
-			True if the light query was valid, false otherwise.
-			
-		Preconditions:
-			None.
-		"""
-		# Make sure the request is a Dictionary.
-		if not isinstance(req, dict):
-			print('Not a dictionary.')
-			return False
-			
-		# Make sure all required keys are present.
-		for key in ['uuid', 'query']:
-			if key not in req.keys():
-				print(key + ' not in req.keys()')
-				return False
-		
-		# Verify the types of the keys' values.
-		if  not isinstance(req['uuid'], str) and	\
-			not isinstance(req['uuid'], unicode):
-			print('uuid not string. Type: '+str(type(req['uuid'])))
-			return False
-		if  not isinstance(req['query'], str) and	\
-			not isinstance(req['query'], unicode):
-			print('query not string. Type: '+str(type(req['query'])))
-			return False
-			
-		# Finally after all that checks out we can return True.
-		return True
-		
-	def _sanitizeStateQuery(self, req):
-		"""
-		Sanitizes a state query. This makes sure that a state query is a
-		JSON Dictionary, then that it has the required keys, and the data 
-		types of those keys' values	are correct.
-		
-		Parameters:
-			req (JSON): The Dictionary that contains the request.
-			
-		Returns:
-			True if the light query was valid, false otherwise.
-			
-		Preconditions:
-			None.
-			
-		Postconditions:
-			None.
-		"""
-		# Make sure the request is a Dictionary.
-		if not isinstance(req, dict):
-			print('Not a dictionary.')
-			return False
-			
-		# Make sure all required keys are present.
-		for key in ['uuid', 'id']:
-			if key not in req.keys():
-				print(key + ' not in req.keys()')
-				return False
-		
-		# Verify the types of the keys' values.
-		if  not isinstance(req['uuid'], str) and	\
-			not isinstance(req['uuid'], unicode):
-			print('uuid not string. Type: '+str(type(req['uuid'])))
-			return False
-		if  not isinstance(req['id'], str) and	\
-			not isinstance(req['id'], unicode):
-			print('id not string. Type: '+str(type(req['id'])))
-			return False
-			
-		# Finally after all that checks out we can return True.
-		return True
-		
-	def _sanitizeAddQuery(self, req):
-		"""
-		Sanitizes a light adding query. This makes sure that the query is a
-		JSON Dictionary, then that it has the required keys, and the data 
-		types of those keys' values	are correct.
-		
-		Parameters:
-			req (JSON): The Dictionary that contains the request.
-			
-		Returns:
-			True if the light query was valid, false otherwise.
-			
-		Preconditions:
-			None.
-			
-		Postconditions:
-			None.
-		"""
-		# Make sure the request is a Dictionary.
-		if not isinstance(req, dict):
-			print('Not a dictionary.')
-			return False
-			
-		# Make sure all required keys are present.
-		for key in ['name', 'client', 'address', 'permitted',
-					'exists', 'id', 'r_c', 'g_c', 'b_c']:
-			if key not in req.keys():
-				print(key + ' not in req.keys()')
-				return False
-		# Verify the types of the keys' values.
-		if  not isinstance(req['name'], str) and	\
-			not isinstance(req['name'], unicode):
-			print('name is not string. Type: '+str(type(req['name'])))
-			return False
-		if  not isinstance(req['client'], str) and	\
-			not isinstance(req['client'], unicode):
-			print('client is not string. Type: '+str(type(req['client'])))
-			return False
-		if  not isinstance(req['address'], str) and	\
-			not isinstance(req['address'], unicode):
-			print('address is not string. Type: '+str(type(req['address'])))
-			return False
-		if  not isinstance(req['permitted'], list):
-			print('permitted is not a list. Type: '+str(type(req['permitted'])))
-			return False
-		if  not isinstance(req['exists'], bool):
-			print('exists is not a boolean. Type: '+str(type(req['exists'])))
-			return False
-		if  req['exists'] and	\
-			not isinstance(req['id'], str) and	\
-			not isinstance(req['id'], unicode):
-			print('id is not a string. Type: '+str(type(req['string'])))
-			return False
-		if  not isinstance(req['r_c'], int):
-			print('r_c is not an integer. Type: '+str(type(req['r_c'])))
-			return False
-		if  not isinstance(req['g_c'], int):
-			print('g_c is not an integer. Type: '+str(type(req['g_c'])))
-			return False
-		if  not isinstance(req['b_c'], int):
-			print('b_c is not an integer. Type: '+str(type(req['b_c'])))
-			return False
-			
-		# Finally after all that checks out we can return True.
-		return True
-		
-	def _sanitizeChangeQuery(self, req):
-		"""
-		Sanitizes a state update request. This makes sure that the form of the
-		object passed as the request is valid for the request. Again, this does
-		key and type testing.
-		
-		Parameters:
-			req (JSON): The Dictionary that contains the request.
-			
-		Returns:
-			True if the light query was valid, false otherwise.
-			
-		Preconditions:
-			None.
-			
-		Postconditions:
-			None.
-		"""
-		if not isinstance(req, dict):
-			return False
-			
-		for key in ['uuid', 'lights']:
-			if key not in req.keys():
-				return False
-		
-		if  not isinstance(req['uuid'],str) and	\
-			not isinstance(req['uuid'],unicode):
-			return False
-		
-		if not isinstance(req['lights'], list):
-			return False
-			
-		return True
-		
-	def _sanitizeLightInfoUpdate(self, req):
-		"""
-		Sanitizes a request to change the info of a light.
-		
-		Parameters:
-			req (JSON): The Dictionary that contains the request.
-			
-		Returns:
-			True if the light query was valid, false otherwise.
-			
-		Preconditions:
-			None.
-			
-		Postconditions:
-			None.
-		"""
-		if not isinstance(req, dict):
-			return False
-			
-		for key in ['id', 'name', 'client', 'permitted']:
-			if key not in req.keys():
-				return False
-		
-		if  not isinstance(req['id'],str) and	\
-			not isinstance(req['id'],unicode):
-			return False
-		
-		if  not isinstance(req['name'],str) and	\
-			not isinstance(req['name'],unicode):
-			return False
-		
-		if  not isinstance(req['client'],str) and	\
-			not isinstance(req['client'],unicode):
-			return False
-		
-		if  not isinstance(req['permitted'],list):
-			return False
-			
-		for id in req['permitted']:
-			if  not isinstance(id,str) and	\
-				not isinstance(id,unicode):
-				return False
-				
-		return True
-		
-	def _sanitizeClientInfoUpdate(self, req):
-		"""
-		Sanitizes a request to change the info of a client.
-		
-		Parameters:
-			req (JSON): The Dictionary that contains the request.
-			
-		Returns:
-			True if the info update was valid, false otherwise.
-			
-		Preconditions:
-			None.
-			
-		Postconditions:
-			None.
-		"""
-		if not isinstance(req, dict):
-			return False
-			
-		for key in ['name', 'address']:
-			if key not in req.keys():
-				return False
-				
-		if  not isinstance(req['name'],str) and	\
-			not isinstance(req['name'],unicode):
-			return False
-		
-		if  not isinstance(req['address'],str) and	\
-			not isinstance(req['address'],unicode):
-			return False
-				
-		return True
-		
 	def lightQuery(self, req):
 		"""
 		Handles a query for light instances.
@@ -335,7 +76,7 @@ class RequestHandler(object):
 			
 		# If the request was invalid, we need to transparently return
 		# nothing.
-		if not self._sanitizeLightQuery(req):
+		if not sanitizeLightQuery(req):
 			print(' Request did not pass sanitation.')
 			return {'lights':[]}
 			
@@ -405,7 +146,7 @@ class RequestHandler(object):
 					'id': None}
 			
 		# Sanitize the request.
-		if not self._sanitizeStateQuery(req):
+		if not sanitizeStateQuery(req):
 			print(' Request did not pass sanitation.')
 			return {'success': False,
 					'message': 'Invalid query.',
@@ -477,7 +218,7 @@ class RequestHandler(object):
 		# Since the light update request is already JSON, we don't
 		# need to worry about parsing it.
 		
-		if not self._sanitizeChangeQuery(req):
+		if not sanitizeChangeQuery(req):
 			print(' Could not decode JSON request.')
 			return {'lights':None,
 					'success': False,
@@ -574,7 +315,7 @@ class RequestHandler(object):
 			
 		# If the request was invalid, we need to transparently return
 		# nothing.
-		if not self._sanitizeAddQuery(req):
+		if not sanitizeAddQuery(req):
 			print(' Request did not pass sanitation.')
 			return {'success':False, 'message':'Request did not pass sanitation. '}
 			
@@ -687,7 +428,7 @@ class RequestHandler(object):
 			return resp
 			
 		# Sanitize the request.
-		if not self._sanitizeLightInfoUpdate(req):
+		if not sanitizeLightInfoUpdate(req):
 			print(' Request did not pass sanitation.')
 			resp['message'] = 'Request did not pass sanitation.'
 			return resp
@@ -791,7 +532,7 @@ class RequestHandler(object):
 			return resp
 			
 		# Sanitize the request.
-		if not self._sanitizeClientInfoUpdate(req):
+		if not sanitizeClientInfoUpdate(req):
 			print(' Request did not pass sanitation.')
 			resp['message'] = 'Request did not pass sanitation.'
 			return resp
---
+++ Server/sanitizer.py
@@ -0,0 +1,266 @@
+#module sanitizer.py
+# LUMA copyright (C) Gerard Geer 2014-2015
+
+"""
+Provides sanitization for all requests.
+"""
+
+def sanitizeLightQuery(req):
+	"""
+	Sanitizes a light query. This makes sure that a light query is a
+	JSON Dictionary, then that it has the required keys, and the data 
+	types of those keys' values	are correct.
+	
+	Parameters:
+		req (JSON): The Dictionary that contains the request.
+		
+	Returns:
+		True if the light query was valid, false otherwise.
+		
+	Preconditions:
+		None.
+	"""
+	# Make sure the request is a Dictionary.
+	if not isinstance(req, dict):
+		print('Not a dictionary.')
+		return False
+		
+	# Make sure all required keys are present.
+	for key in ['uuid', 'query']:
+		if key not in req.keys():
+			print(key + ' not in req.keys()')
+			return False
+	
+	# Verify the types of the keys' values.
+	if  not isinstance(req['uuid'], str) and	\
+		not isinstance(req['uuid'], unicode):
+		print('uuid not string. Type: '+str(type(req['uuid'])))
+		return False
+	if  not isinstance(req['query'], str) and	\
+		not isinstance(req['query'], unicode):
+		print('query not string. Type: '+str(type(req['query'])))
+		return False
+		
+	# Finally after all that checks out we can return True.
+	return True
+	
+def sanitizeStateQuery(req):
+	"""
+	Sanitizes a state query. This makes sure that a state query is a
+	JSON Dictionary, then that it has the required keys, and the data 
+	types of those keys' values	are correct.
+	
+	Parameters:
+		req (JSON): The Dictionary that contains the request.
+		
+	Returns:
+		True if the light query was valid, false otherwise.
+		
+	Preconditions:
+		None.
+		
+	Postconditions:
+		None.
+	"""
+	# Make sure the request is a Dictionary.
+	if not isinstance(req, dict):
+		print('Not a dictionary.')
+		return False
+		
+	# Make sure all required keys are present.
+	for key in ['uuid', 'id']:
+		if key not in req.keys():
+			print(key + ' not in req.keys()')
+			return False
+	
+	# Verify the types of the keys' values.
+	if  not isinstance(req['uuid'], str) and	\
+		not isinstance(req['uuid'], unicode):
+		print('uuid not string. Type: '+str(type(req['uuid'])))
+		return False
+	if  not isinstance(req['id'], str) and	\
+		not isinstance(req['id'], unicode):
+		print('id not string. Type: '+str(type(req['id'])))
+		return False
+		
+	# Finally after all that checks out we can return True.
+	return True
+	
+def sanitizeAddQuery(req):
+	"""
+	Sanitizes a light adding query. This makes sure that the query is a
+	JSON Dictionary, then that it has the required keys, and the data 
+	types of those keys' values	are correct.
+	
+	Parameters:
+		req (JSON): The Dictionary that contains the request.
+		
+	Returns:
+		True if the light query was valid, false otherwise.
+		
+	Preconditions:
+		None.
+		
+	Postconditions:
+		None.
+	"""
+	# Make sure the request is a Dictionary.
+	if not isinstance(req, dict):
+		print('Not a dictionary.')
+		return False
+		
+	# Make sure all required keys are present.
+	for key in ['name', 'client', 'address', 'permitted',
+				'exists', 'id', 'r_c', 'g_c', 'b_c']:
+		if key not in req.keys():
+			print(key + ' not in req.keys()')
+			return False
+	# Verify the types of the keys' values.
+	if  not isinstance(req['name'], str) and	\
+		not isinstance(req['name'], unicode):
+		print('name is not string. Type: '+str(type(req['name'])))
+		return False
+	if  not isinstance(req['client'], str) and	\
+		not isinstance(req['client'], unicode):
+		print('client is not string. Type: '+str(type(req['client'])))
+		return False
+	if  not isinstance(req['address'], str) and	\
+		not isinstance(req['address'], unicode):
+		print('address is not string. Type: '+str(type(req['address'])))
+		return False
+	if  not isinstance(req['permitted'], list):
+		print('permitted is not a list. Type: '+str(type(req['permitted'])))
+		return False
+	if  not isinstance(req['exists'], bool):
+		print('exists is not a boolean. Type: '+str(type(req['exists'])))
+		return False
+	if  req['exists'] and	\
+		not isinstance(req['id'], str) and	\
+		not isinstance(req['id'], unicode):
+		print('id is not a string. Type: '+str(type(req['string'])))
+		return False
+	if  not isinstance(req['r_c'], int):
+		print('r_c is not an integer. Type: '+str(type(req['r_c'])))
+		return False
+	if  not isinstance(req['g_c'], int):
+		print('g_c is not an integer. Type: '+str(type(req['g_c'])))
+		return False
+	if  not isinstance(req['b_c'], int):
+		print('b_c is not an integer. Type: '+str(type(req['b_c'])))
+		return False
+		
+	# Finally after all that checks out we can return True.
+	return True
+	
+def sanitizeChangeQuery(req):
+	"""
+	Sanitizes a state update request. This makes sure that the form of the
+	object passed as the request is valid for the request. Again, this does
+	key and type testing.
+	
+	Parameters:
+		req (JSON): The Dictionary that contains the request.
+		
+	Returns:
+		True if the light query was valid, false otherwise.
+		
+	Preconditions:
+		None.
+		
+	Postconditions:
+		None.
+	"""
+	if not isinstance(req, dict):
+		return False
+		
+	for key in ['uuid', 'lights']:
+		if key not in req.keys():
+			return False
+	
+	if  not isinstance(req['uuid'],str) and	\
+		not isinstance(req['uuid'],unicode):
+		return False
+	
+	if not isinstance(req['lights'], list):
+		return False
+		
+	return True
+	
+def sanitizeLightInfoUpdate(req):
+	"""
+	Sanitizes a request to change the info of a light.
+	
+	Parameters:
+		req (JSON): The Dictionary that contains the request.
+		
+	Returns:
+		True if the light query was valid, false otherwise.
+		
+	Preconditions:
+		None.
+		
+	Postconditions:
+		None.
+	"""
+	if not isinstance(req, dict):
+		return False
+		
+	for key in ['id', 'name', 'client', 'permitted']:
+		if key not in req.keys():
+			return False
+	
+	if  not isinstance(req['id'],str) and	\
+		not isinstance(req['id'],unicode):
+		return False
+	
+	if  not isinstance(req['name'],str) and	\
+		not isinstance(req['name'],unicode):
+		return False
+	
+	if  not isinstance(req['client'],str) and	\
+		not isinstance(req['client'],unicode):
+		return False
+	
+	if  not isinstance(req['permitted'],list):
+		return False
+		
+	for id in req['permitted']:
+		if  not isinstance(id,str) and	\
+			not isinstance(id,unicode):
+			return False
+			
+	return True
+	
+def sanitizeClientInfoUpdate(req):
+	"""
+	Sanitizes a request to change the info of a client.
+	
+	Parameters:
+		req (JSON): The Dictionary that contains the request.
+		
+	Returns:
+		True if the info update was valid, false otherwise.
+		
+	Preconditions:
+		None.
+		
+	Postconditions:
+		None.
+	"""
+	if not isinstance(req, dict):
+		return False
+		
+	for key in ['name', 'address']:
+		if key not in req.keys():
+			return False
+			
+	if  not isinstance(req['name'],str) and	\
+		not isinstance(req['name'],unicode):
+		return False
+	
+	if  not isinstance(req['address'],str) and	\
+		not isinstance(req['address'],unicode):
+		return False
+			
+	return True
+	
